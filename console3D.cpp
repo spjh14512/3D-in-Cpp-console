9460e0d3eba2bf3366c6dcd50c3920d936a26da7
spjh14512 <127752566+spjh14512@users.noreply.github.com>
2024-04-05 23:15:29 +0900

    camera added

--- console3D.cpp
+++ console3D.cpp
@@ -4,15 +4,36 @@
 #include <windows.h>
 #include <vector>
 #include <memory.h>
+#include <conio.h>
 
 
 #define SCREEN_WIDTH 150
 #define SCREEN_HEIGHT 50
+#define ARROW_KEYS -32
+#define UP 72
+#define DOWN 80
+#define LEFT 75
+#define RIGHT 77
+
+
+HANDLE std_output_handle = GetStdHandle(STD_OUTPUT_HANDLE);
 enum {x_axis, y_axis, z_axis};
 
 using namespace std;
 
-bool screen[SCREEN_HEIGHT][SCREEN_WIDTH];
+short screen[SCREEN_HEIGHT][SCREEN_WIDTH];
+enum {EMPTY = 0, FRONT, BACK };
+
+const double fovy = M_PI / 3;
+const double aspect = 1;
+const double f = 50;
+const double n = 10;
+const double camera_sensitivity = M_PI / 60;
+const double camera_move_speed = 2;
+
+class vec4;
+class mat4;
+class Object;
 
 class vec4 {
 public:
@@ -27,6 +48,7 @@ public:
 	double operator*(const vec4& other) { return x * other.x + y * other.y + z * other.z; }
 	
 };
+
 
 class mat4 {
 public:
@@ -54,28 +76,6 @@ public:
 		for (int i = 0; i < 16; i++) this->elements[i] -= rhs.elements[i];
 		return *this;
 	}
-	inline mat4& operator*=(const mat4& rhs) {
-		this->elements[0] = this->elements[0] * rhs.elements[0] + this->elements[1] * rhs.elements[4] + this->elements[2] * rhs.elements[8] + this->elements[3] * rhs.elements[12];
-		this->elements[1] = this->elements[0] * rhs.elements[1] + this->elements[1] * rhs.elements[5] + this->elements[2] * rhs.elements[9] + this->elements[3] * rhs.elements[13];
-		this->elements[2] = this->elements[0] * rhs.elements[2] + this->elements[1] * rhs.elements[6] + this->elements[2] * rhs.elements[10] + this->elements[3] * rhs.elements[14];
-		this->elements[3] = this->elements[0] * rhs.elements[3] + this->elements[1] * rhs.elements[7] + this->elements[2] * rhs.elements[11] + this->elements[3] * rhs.elements[15];
-
-		this->elements[4] = this->elements[4] * rhs.elements[0] + this->elements[5] * rhs.elements[4] + this->elements[6] * rhs.elements[8] + this->elements[7] * rhs.elements[12];
-		this->elements[5] = this->elements[4] * rhs.elements[1] + this->elements[5] * rhs.elements[5] + this->elements[6] * rhs.elements[9] + this->elements[7] * rhs.elements[13];
-		this->elements[6] = this->elements[4] * rhs.elements[2] + this->elements[5] * rhs.elements[6] + this->elements[6] * rhs.elements[10] + this->elements[7] * rhs.elements[14];
-		this->elements[7] = this->elements[4] * rhs.elements[3] + this->elements[5] * rhs.elements[7] + this->elements[6] * rhs.elements[11] + this->elements[7] * rhs.elements[15];
-
-		this->elements[8] = this->elements[8] * rhs.elements[0] + this->elements[9] * rhs.elements[4] + this->elements[10] * rhs.elements[8] + this->elements[11] * rhs.elements[12];
-		this->elements[9] = this->elements[8] * rhs.elements[1] + this->elements[9] * rhs.elements[5] + this->elements[10] * rhs.elements[9] + this->elements[11] * rhs.elements[13];
-		this->elements[10] = this->elements[8] * rhs.elements[2] + this->elements[9] * rhs.elements[6] + this->elements[10] * rhs.elements[10] + this->elements[11] * rhs.elements[14];
-		this->elements[11] = this->elements[8] * rhs.elements[3] + this->elements[9] * rhs.elements[7] + this->elements[10] * rhs.elements[11] + this->elements[11] * rhs.elements[15];
-
-		this->elements[12] = this->elements[12] * rhs.elements[0] + this->elements[13] * rhs.elements[4] + this->elements[14] * rhs.elements[8] + this->elements[15] * rhs.elements[12];
-		this->elements[13] = this->elements[12] * rhs.elements[1] + this->elements[13] * rhs.elements[5] + this->elements[14] * rhs.elements[9] + this->elements[15] * rhs.elements[13];
-		this->elements[14] = this->elements[12] * rhs.elements[2] + this->elements[13] * rhs.elements[6] + this->elements[14] * rhs.elements[10] + this->elements[15] * rhs.elements[14];
-		this->elements[15] = this->elements[12] * rhs.elements[3] + this->elements[13] * rhs.elements[7] + this->elements[14] * rhs.elements[11] + this->elements[15] * rhs.elements[15];
-		return *this;
-	}
 	inline mat4& operator*=(const double& k) {
 		for (int i = 0; i < 16; i++) this->elements[i] *= k;
 		return *this;
@@ -93,7 +93,26 @@ public:
 	}
 	inline mat4 operator*(const mat4& rhs) {
 		mat4 newMat(*this);
-		newMat *= rhs;
+		newMat.elements[0] = this->elements[0] * rhs.elements[0] + this->elements[1] * rhs.elements[4] + this->elements[2] * rhs.elements[8] + this->elements[3] * rhs.elements[12];
+		newMat.elements[1] = this->elements[0] * rhs.elements[1] + this->elements[1] * rhs.elements[5] + this->elements[2] * rhs.elements[9] + this->elements[3] * rhs.elements[13];
+		newMat.elements[2] = this->elements[0] * rhs.elements[2] + this->elements[1] * rhs.elements[6] + this->elements[2] * rhs.elements[10] + this->elements[3] * rhs.elements[14];
+		newMat.elements[3] = this->elements[0] * rhs.elements[3] + this->elements[1] * rhs.elements[7] + this->elements[2] * rhs.elements[11] + this->elements[3] * rhs.elements[15];
+
+		newMat.elements[4] = this->elements[4] * rhs.elements[0] + this->elements[5] * rhs.elements[4] + this->elements[6] * rhs.elements[8] + this->elements[7] * rhs.elements[12];
+		newMat.elements[5] = this->elements[4] * rhs.elements[1] + this->elements[5] * rhs.elements[5] + this->elements[6] * rhs.elements[9] + this->elements[7] * rhs.elements[13];
+		newMat.elements[6] = this->elements[4] * rhs.elements[2] + this->elements[5] * rhs.elements[6] + this->elements[6] * rhs.elements[10] + this->elements[7] * rhs.elements[14];
+		newMat.elements[7] = this->elements[4] * rhs.elements[3] + this->elements[5] * rhs.elements[7] + this->elements[6] * rhs.elements[11] + this->elements[7] * rhs.elements[15];
+
+		newMat.elements[8] = this->elements[8] * rhs.elements[0] + this->elements[9] * rhs.elements[4] + this->elements[10] * rhs.elements[8] + this->elements[11] * rhs.elements[12];
+		newMat.elements[9] = this->elements[8] * rhs.elements[1] + this->elements[9] * rhs.elements[5] + this->elements[10] * rhs.elements[9] + this->elements[11] * rhs.elements[13];
+		newMat.elements[10] = this->elements[8] * rhs.elements[2] + this->elements[9] * rhs.elements[6] + this->elements[10] * rhs.elements[10] + this->elements[11] * rhs.elements[14];
+		newMat.elements[11] = this->elements[8] * rhs.elements[3] + this->elements[9] * rhs.elements[7] + this->elements[10] * rhs.elements[11] + this->elements[11] * rhs.elements[15];
+
+		newMat.elements[12] = this->elements[12] * rhs.elements[0] + this->elements[13] * rhs.elements[4] + this->elements[14] * rhs.elements[8] + this->elements[15] * rhs.elements[12];
+		newMat.elements[13] = this->elements[12] * rhs.elements[1] + this->elements[13] * rhs.elements[5] + this->elements[14] * rhs.elements[9] + this->elements[15] * rhs.elements[13];
+		newMat.elements[14] = this->elements[12] * rhs.elements[2] + this->elements[13] * rhs.elements[6] + this->elements[14] * rhs.elements[10] + this->elements[15] * rhs.elements[14];
+		newMat.elements[15] = this->elements[12] * rhs.elements[3] + this->elements[13] * rhs.elements[7] + this->elements[14] * rhs.elements[11] + this->elements[15] * rhs.elements[15];
+
 		return newMat;
 	}
 	inline vec4 operator*(const vec4& vec) {
@@ -104,6 +123,18 @@ public:
 		return vec4(_x, _y, _z, _w);
 	}
 
+	inline mat4& transpose() {
+		mat4 newMat;
+		newMat.elements[0] = elements[0]; newMat.elements[1] = elements[4];
+		newMat.elements[2] = elements[8]; newMat.elements[3] = elements[12];
+		newMat.elements[4] = elements[1]; newMat.elements[5] = elements[5];
+		newMat.elements[6] = elements[9]; newMat.elements[7] = elements[13];
+		newMat.elements[8] = elements[2]; newMat.elements[9] = elements[6];
+		newMat.elements[10] = elements[10]; newMat.elements[11] = elements[14];
+		newMat.elements[12] = elements[3]; newMat.elements[13] = elements[7];
+		newMat.elements[14] = elements[11]; newMat.elements[15] = elements[15];
+		return newMat;
+	}
 	inline mat4& normarlize() {
 		for (int col = 0; col < 4; col++) {
 			double k = sqrt(pow(elements[col], 2) + pow(elements[col + 4], 2) + pow(elements[col + 8], 2) + pow(elements[col + 12], 2));
@@ -115,20 +146,18 @@ public:
 		return *this;
 	}
 
+	void print() {
+		for (int i = 0; i < 4; i++) {
+			for (int j = 0; j < 4; j++) {
+				printf("%.8lf  ", elements[i * 4 + j]);
+			}
+			cout << endl;
+		}
+		cout << endl;
+	}
+
 };
 
-inline mat4 transpose(const mat4& mat) {
-	mat4 newMat;
-	newMat.elements[0] = mat.elements[0]; newMat.elements[1] = mat.elements[4];
-	newMat.elements[2] = mat.elements[8]; newMat.elements[3] = mat.elements[12];
-	newMat.elements[4] = mat.elements[1]; newMat.elements[5] = mat.elements[5];
-	newMat.elements[6] = mat.elements[9]; newMat.elements[7] = mat.elements[13];
-	newMat.elements[8] = mat.elements[2]; newMat.elements[9] = mat.elements[6];
-	newMat.elements[10] = mat.elements[10]; newMat.elements[11] = mat.elements[14];
-	newMat.elements[12] = mat.elements[3]; newMat.elements[13] = mat.elements[7];
-	newMat.elements[14] = mat.elements[11]; newMat.elements[15] = mat.elements[15];
-	return newMat;
-}
 mat4 createAffineWSRotationMat4(const short axis, double angle) {
 	mat4 newMat4;
 	double s = sin(angle);
@@ -158,6 +187,16 @@ mat4 createAffineTranslationMat4(const vec4& vec) {
 	newMat4.elements[11] = vec.z;
 	return newMat4;
 }
+mat4 createAffineScalingMat4(const vec4& vec) {
+	mat4 newMat;
+	newMat.elements[0] = vec.x;
+	newMat.elements[5] = vec.y;
+	newMat.elements[10] = vec.z;
+	return newMat;
+}
+mat4 createViewTransformMat4(Object camera);
+mat4 createProjectionTransformMat4();
+mat4 createViewportTransformMat4();
 
 
 class Object {
@@ -173,78 +212,179 @@ public:
 	}
 	void rotateOS(const short axis, double angle) {
 		vec4 t(pose.elements[3], pose.elements[7], pose.elements[11]);
-		mat4 R_inv(pose);
-		R_inv.elements[3] = R_inv.elements[7] = R_inv.elements[11] = 0;
-		mat4 R = transpose(R_inv);
+		mat4 R(pose);
+		R.elements[3] = R.elements[7] = R.elements[11] = 0;
 
-		this->translate(t * -1);
-		pose = R * pose;
+		pose = mat4();
 		this->rotateWS(axis, angle);
-		pose = R_inv * pose;
-		this->translate(t);
+		pose = R * pose;
+		pose.normarlize();
+		this->translateWS(t);
 		return;
 	}
-	void translate(const vec4& vec) {
+	void translateWS(const vec4& vec) {
 		mat4 t = createAffineTranslationMat4(vec);
 		pose = t * pose;
 		return;
 	}
-	void drawOnScreen() {
+	void translateOS(const vec4& vec) {
+		vec4 t(pose.elements[3], pose.elements[7], pose.elements[11]);
+		mat4 R(pose);
+		R.elements[3] = R.elements[7] = R.elements[11] = 0;
+
+		pose = mat4();
+		this->translateWS(vec);
+		pose = R * pose;
+		this->translateWS(t);
+
+	}
+	void scale(const vec4& vec) {
+		mat4 S = createAffineScalingMat4(vec);
+		pose = S * pose;
+	}
+	void drawOnScreen(Object camera) {
+		mat4 vt = createViewTransformMat4(camera);
+		mat4 pt = createProjectionTransformMat4();
+		mat4 vpt = createViewportTransformMat4();
+
 		for (const vec4& vertex : vertices) {
-			vec4 _vertex = pose * vertex;
-			screen[(int)round(SCREEN_HEIGHT - _vertex.y)][(int)round(_vertex.x)] = true;
+			vec4 _vertex = pt * (vt * (pose * vertex));
+			if (_vertex.z < 0) continue;
+			int distance_color = _vertex.z < 40 ? FRONT : BACK;
+			_vertex = _vertex * (1 / _vertex.z);
+			_vertex = vpt * _vertex;
+			int r = (int)round( _vertex.y);
+			int c = (int)round(_vertex.x);
+
+			if (r < 0 || r >= SCREEN_HEIGHT || c < 0 || c >= SCREEN_WIDTH) continue;
+			screen[r][c] |= distance_color;
 		}
 	}
 };
 
+mat4 createViewTransformMat4(Object camera) {
+	vec4 EYE(camera.pose.elements[3], camera.pose.elements[7], camera.pose.elements[11]);
+	mat4 R(camera.pose);
+	R.elements[3] = R.elements[7] = R.elements[11] = 0;
+	mat4 R_inv = R.transpose();
+
+	mat4 T_inv = createAffineTranslationMat4(EYE * -1);
+
+	return R_inv * T_inv;
+}
+mat4 createProjectionTransformMat4() {
+	mat4 newMat;
+	newMat.elements[0] = 1 / (aspect * tan(fovy / 2));
+	newMat.elements[5] = 1 / tan(fovy / 2);
+	newMat.elements[10] = -(f + n) / (f - n);
+	newMat.elements[11] = -(2 * n * f) / (f - n);
+	newMat.elements[14] = -1;
+	newMat.elements[15] = 0;
+	return newMat;
+}
+mat4 createViewportTransformMat4() {
+	mat4 newMat = createAffineScalingMat4(vec4(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, 0.5));
+	newMat.elements[3] = SCREEN_WIDTH / 2;
+	newMat.elements[7] = SCREEN_HEIGHT / 2;
+	newMat.elements[11] = 0.5;
+	return newMat;
+}
+
 void clearScreen() {
-	memset(screen, false, sizeof(screen));
-	system("cls");
+	memset(screen, 0, sizeof(screen));
+	COORD pos = { 0, 0 };
+	SetConsoleCursorPosition(std_output_handle, pos);
 }
 
 void drawScreen() {
 	for (int i = 0; i < SCREEN_HEIGHT; i++) {
 		for (int j = 0; j < SCREEN_WIDTH; j++) {
-			screen[i][j] ? printf("@") : printf("_");
+			if (screen[i][j] == 0) {
+				printf(" ");
+			}
+			else if (screen[i][j] & 1) {
+				SetConsoleTextAttribute(std_output_handle, 15);
+				printf("@");
+			}
+			else {
+				SetConsoleTextAttribute(std_output_handle, 8);
+				printf("#");
+			}
 		}
 		printf("\n");
 	}
 }
 
+Object obj_cube(const int cube_size, int x, int y, int z) {
+	Object cube;
+	const int half = (int)cube_size / 2;
+	for (double i = -half; i <= half; i += 0.5) cube.vertices.push_back(vec4(i, -half, -half));
+	for (double i = -half; i <= half; i += 0.5) cube.vertices.push_back(vec4(i, half, -half));
+	for (double i = -half; i <= half; i += 0.5) cube.vertices.push_back(vec4(i, half, half));
+	for (double i = -half; i <= half; i += 0.5) cube.vertices.push_back(vec4(i, -half, half));
+	for (double i = -half; i <= half; i += 0.5) cube.vertices.push_back(vec4(-half, i, -half));
+	for (double i = -half; i <= half; i += 0.5) cube.vertices.push_back(vec4(half, i, -half));
+	for (double i = -half; i <= half; i += 0.5) cube.vertices.push_back(vec4(half, i, half));
+	for (double i = -half; i <= half; i += 0.5) cube.vertices.push_back(vec4(-half, i, half));
+	for (double i = -half; i <= half; i += 0.5) cube.vertices.push_back(vec4(-half, -half, i));
+	for (double i = -half; i <= half; i += 0.5) cube.vertices.push_back(vec4(half, -half, i));
+	for (double i = -half; i <= half; i += 0.5) cube.vertices.push_back(vec4(half, half, i));
+	for (double i = -half; i <= half; i += 0.5) cube.vertices.push_back(vec4(-half, half, i));
+	cube.translateWS(vec4(x, y, z));
+	return cube;
+}
+
 int main() {
-	/*Object cube;
-	for (int i = -5; i < 6; i++) cube.vertices.push_back(vec4(i, -5, -5));
-	for (int i = -5; i < 6; i++) cube.vertices.push_back(vec4(i, 5, -5));
-	for (int i = -5; i < 6; i++) cube.vertices.push_back(vec4(i, 5, 5));
-	for (int i = -5; i < 6; i++) cube.vertices.push_back(vec4(i, -5, 5));
-	for (int i = -5; i < 6; i++) cube.vertices.push_back(vec4(-5, i, -5));
-	for (int i = -5; i < 6; i++) cube.vertices.push_back(vec4(5, i, -5));
-	for (int i = -5; i < 6; i++) cube.vertices.push_back(vec4(5, i, 5));
-	for (int i = -5; i < 6; i++) cube.vertices.push_back(vec4(-5, i, 5));
-	for (int i = -5; i < 6; i++) cube.vertices.push_back(vec4(-5, -5, i));
-	for (int i = -5; i < 6; i++) cube.vertices.push_back(vec4(5, -5, i));
-	for (int i = -5; i < 6; i++) cube.vertices.push_back(vec4(5, 5, i));
-	for (int i = -5; i < 6; i++) cube.vertices.push_back(vec4(-5, 5, i));
 
-	cube.translate(vec4(75, 25, 20));
+	CONSOLE_CURSOR_INFO cursorInfo = { 0, };
+	cursorInfo.bVisible = FALSE; //커서 Visible TRUE(보임) FALSE(숨김)
+	SetConsoleCursorInfo(std_output_handle, &cursorInfo);
+	char kb_input;
+
+	/* Create Object */
+
+	Object camera;
+	Object cube1 = obj_cube(29, 0, 0, -50);
+
+	/* End */
 
 	while (1) {
-		clearScreen();
-		cube.drawOnScreen();
-		drawScreen();
-		cube.rotateOS(y_axis, M_PI / 60);
-		cube.rotateOS(x_axis, M_PI / 60);
-		Sleep(500);
-	}*/
+		/* Draw */
 
-	Object stick;
-	for (int i = -7; i < 8; i++) stick.vertices.push_back(vec4(0, i, 0));
-	stick.translate(vec4(75, 25, 0));
-	while (1) {
 		clearScreen();
-		stick.drawOnScreen();
+		cube1.drawOnScreen(camera);
 		drawScreen();
-		stick.translate(vec4(1, 0, 0));
-		Sleep(100);
+
+		/* Auto actions per frame */
+		
+		cube1.rotateOS(y_axis, M_PI / 60);
+
+
+		/* Control */
+		if (_kbhit()) {
+			kb_input = _getch();
+			if (kb_input == ARROW_KEYS) {
+				kb_input = _getch();
+				switch (kb_input) {
+				case LEFT:
+					camera.rotateOS(y_axis, camera_sensitivity);
+					break;
+				case RIGHT:
+					camera.rotateOS(y_axis, -camera_sensitivity);
+					break;
+				case UP:
+					camera.rotateOS(x_axis, -camera_sensitivity);
+					break;
+				case DOWN:
+					camera.rotateOS(x_axis, camera_sensitivity);
+					break;
+				}
+			}
+			else if (kb_input == 97) camera.translateOS(vec4(-camera_move_speed, 0, 0)); // LEFT
+			else if (kb_input == 100) camera.translateOS(vec4(camera_move_speed, 0, 0)); // RIGHT
+			else if (kb_input == 119) camera.translateOS(vec4(0, 0, -camera_move_speed)); // FORWARD
+			else if (kb_input == 115) camera.translateOS(vec4(0, 0, camera_move_speed)); // BACKWARD
+		}
 	}
+
 }
